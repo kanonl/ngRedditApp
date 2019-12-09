9583e212b00bf1a61bfe6ff78b1db1d0f4aa077b
Kanon Lo <15108745+kanonl@users.noreply.github.com>
2019-12-09 10:34:03 -0500

    Typo fix

--- src/app/service/reddit.service.ts
+++ src/app/service/reddit.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ExecFileOptionsWithStringEncoding } from 'child_process';
 
 @Injectable({
   providedIn: 'root'
